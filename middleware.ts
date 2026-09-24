ec74ec1ad2c656887769845dfda804b81992e7f3
20220255 <118017744+20220255@users.noreply.github.com>
2026-09-25 03:02:39 +0800

    Fix on Auth 2

--- middleware.ts
+++ middleware.ts
@@ -43,9 +43,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/",
-    "/((?!_next|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
-    "/.well-known/(.*)",
+    "/__clerk/(.*)",
   ],
 };
